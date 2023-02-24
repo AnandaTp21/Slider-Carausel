2565ec94c94c4f37bfa3e397322847955afa4818
AnandaTp21 <ananda21062002@gmail.com>
2023-02-24 13:28:45 +0700

    add slider-banner

--- src/slider.jsx
+++ src/slider.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 function Slider() {
-  const data = [{img : "./pepsi.png",title : "Pepsi"}, {img : "./sprite.png",title : "Sprite"}, {img : "./cocacola.png",title : "Coca-Cola"}];
+  const data = [
+    { img: "./pepsi.png", title: "Pepsi" },
+    { img: "./sprite.png", title: "Sprite" },
+    { img: "./cocacola.png", title: "Coca-Cola" },
+  ];
   const [active, setActive] = useState([false, false, false]);
   const [counter, setCounter] = useState(0);
   useEffect(() => {
@@ -12,7 +16,7 @@ function Slider() {
         if (counter === 0) {
           Slider.style.background = "linear-gradient(to right ,blue,red)";
         } else if (counter === 1) {
-          Slider.style.background ="linear-gradient(to right ,yellow,green)";
+          Slider.style.background = "linear-gradient(to right ,yellow,green)";
         } else if (counter === 2) {
           Slider.style.background = "linear-gradient(to right ,black,red)";
         }
@@ -24,47 +28,86 @@ function Slider() {
   }, [counter]);
   return (
     <>
-      <button id="button"
-        onClick={() => {
-          if (counter === 0) {
-            setCounter(2);
-          } else {
-            setCounter(counter - 1);
-          }
-        }}
-      >
-        left
-      </button>
-      <div className="slider">
-        
-          {data.map((data,idx) => (
-            <>{<img src={data.img} className ={`${active[idx] && "active"}`} style = {{
-              position : "absolute",
-              left : "-300px",
-              transition : "1s"
-            }} />}</>
+      <div className="App2">
+        <button
+          id="button"
+          onClick={() => {
+            if (counter === 0) {
+              setCounter(2);
+            } else {
+              setCounter(counter - 1);
+            }
+          }}
+        >
+          left
+        </button>
+        <div className="slider">
+          {data.map((data, idx) => (
+            <>
+              {
+                <img
+                  src={data.img}
+                  className={`${active[idx] && "active"}`}
+                  style={{
+                    position: "absolute",
+                    left: "-300px",
+                    transition: "1s",
+                  }}
+                />
+              }
+            </>
           ))}
-              {data.map((data,idx)=>(
-                <div className ={`${active[idx] && "active"} text`} style={{transition : "1s",width : "500px",position : "absolute", right : "-500px"}}>
-                  <h1 style={{marginTop : "0px"}}>{data.title}</h1>
-                  <p style={{textAlign : "left"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi veritatis amet cupiditate nulla deserunt provident ipsum blanditiis ut reiciendis. Corporis, reiciendis adipisci recusandae doloribus possimus quo voluptatem debitis voluptate fuga?</p>
-                  <p style={{textAlign : "left"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, iste aperiam eveniet nulla quaerat minus, accusantium at voluptatibus rerum dolor natus! Alias, dicta. Blanditiis, voluptates iure necessitatibus optio voluptatem expedita?</p>
-                </div>
-            ))}
+          {data.map((data, idx) => (
+            <div
+              className={`${active[idx] && "active"} text`}
+              style={{
+                transition: "1s",
+                width: "500px",
+                position: "absolute",
+                right: "-500px",
+              }}
+            >
+              <h1 style={{ marginTop: "0px" }}>{data.title}</h1>
+              <p style={{ textAlign: "left" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+                veritatis amet cupiditate nulla deserunt provident ipsum
+                blanditiis ut reiciendis. Corporis, reiciendis adipisci
+                recusandae doloribus possimus quo voluptatem debitis voluptate
+                fuga?
+              </p>
+              <p style={{ textAlign: "left" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
+                iste aperiam eveniet nulla quaerat minus, accusantium at
+                voluptatibus rerum dolor natus! Alias, dicta. Blanditiis,
+                voluptates iure necessitatibus optio voluptatem expedita?
+              </p>
+            </div>
+          ))}
+        </div>
+        <button
+          id="button"
+          onClick={() => {
+            if (counter === 2) {
+              setCounter(0);
+            } else {
+              setCounter(counter + 1);
+            }
+          }}
+        >
+          right
+        </button>
       </div>
-      <button id="button"
-        onClick={() => {
-          if (counter === 2) {
-            setCounter(0);
-          } else {
-            setCounter(counter + 1);
-          }
-        }}
-      >
-        right
-      </button >
+      <div className="active-banner">
+        {active.map((val,idx)=>(
+          <div onClick={()=>{
+            setCounter(idx)
+          }} className={`circle ${val && "active"}`}>
+          </div>
+        ))}
+      </div>
     </>
   );
+
 }
 
 export default Slider;
